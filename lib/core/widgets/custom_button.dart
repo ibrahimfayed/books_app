1236a7fe13af8c241969fe0a56354014e9971fb4
Your Name <your_email@example.com>
2025-02-18 23:57:53 +0200

    create box_action_button

---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,41 @@
+import 'package:books_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+   super.key,
+   required this.backgroundColor,
+   required this.textcolor,  
+   required this.text,
+   this.fontsize,
+   this.borderRadius,
+    });
+
+final Color backgroundColor;
+final Color textcolor;
+final BorderRadius? borderRadius;
+final double? fontsize;
+final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return  SizedBox(
+      height: 48,
+      child: TextButton(
+        onPressed: (){},
+        style: TextButton.styleFrom(
+          backgroundColor: backgroundColor,
+          shape: RoundedRectangleBorder(
+            borderRadius:borderRadius ?? BorderRadius.circular(16)
+          )
+        ),
+        child: Text(text,
+        
+        style: Styles.textstyle18.copyWith(
+          color: textcolor,
+          fontSize: fontsize,
+          fontWeight: FontWeight.w900),),
+      ),
+    );
+  }
+}
---
+++ lib/features/home/presentaion/views/widgets/books_action.dart
@@ -0,0 +1,38 @@
+import 'package:books_app/core/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
+
+class BooksAction extends StatelessWidget {
+  const BooksAction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding:  EdgeInsets.symmetric(horizontal: 8),
+      child:  Row(
+        children: [
+          Expanded(child: CustomButton(
+            text: '19.99€',
+            backgroundColor: Colors.white,
+            textcolor: Colors.black,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(12),
+              bottomRight: Radius.circular(12)
+            ),
+          ),
+          ),
+          Expanded(child: CustomButton(
+            text: 'Free preview',
+            fontsize: 16,
+            backgroundColor: Color(0xffEF8262),
+            textcolor: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(12),
+              bottomRight: Radius.circular(12)
+            ),
+          ),
+          )
+        ],
+      ),
+    );
+  }
+}
