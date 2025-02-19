9e21b737f341198add0b6633e3af1cae3580185d
Your Name <your_email@example.com>
2025-02-19 20:54:32 +0200

    create a wrapper around api

---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,19 @@
+import 'package:books_app/core/errors/failures.dart';
+import 'package:books_app/features/home/data/models/books_model/books_model.dart';
+import 'package:books_app/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  @override
+  Future<Either<Failure, List<BooksModel>>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BooksModel>>> fetchFeaturesBooks() {
+    // TODO: implement fetchFeaturesBooks
+    throw UnimplementedError();
+  }
+  
+}
