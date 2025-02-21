adb80e33986a68f6bc9c8a45019cf82d0a0e6b7b
Your Name <your_email@example.com>
2025-02-21 23:22:42 +0200

    create featured books methoud

--- lib/features/home/presentaion/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentaion/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bloc/bloc.dart';
+import 'package:books_app/core/errors/failures.dart';
 import 'package:books_app/features/home/data/models/books_model/books_model.dart';
+import 'package:books_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
+  
+  Future<void>featchFeaturedBooks()async{
+    emit(FeaturedBooksLoading());
+   var result = await homeRepo.fetchFeaturesBooks();
+   result.fold((failure){
+    emit(FeaturedBooksFailure(failure.errmessage));
+   }, (books){
+    emit(FeaturedBooksSuccess(books));
+   });
+
+  }
 }
