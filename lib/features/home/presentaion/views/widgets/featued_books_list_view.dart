e69db780d9439acbd075d499cca710a849b8d71b
Your Name <your_email@example.com>
2025-02-23 00:29:46 +0200

    display featured images finally part2

--- lib/features/home/presentaion/views/widgets/featued_books_list_view.dart
+++ lib/features/home/presentaion/views/widgets/featued_books_list_view.dart
@@ -16,11 +16,15 @@ class FeaturedBooksListView extends StatelessWidget {
   return SizedBox(
     height: MediaQuery.of(context).size.height * 0.25,
     child: ListView.builder(
+      physics: const BouncingScrollPhysics(),
+      itemCount: state.books.length,//it solves the exception of range error 
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8),
-            child: CustomBookImage(),
+          return  Padding(
+            padding:const EdgeInsets.symmetric(horizontal: 8),
+            child: CustomBookImage(
+              imageUrl: state.books[index].volumeInfo.imageLinks.thumbnail,
+            ),
           );
         }),
   );
--- lib/features/home/presentaion/views/widgets/similar_books_list_view.dart
+++ lib/features/home/presentaion/views/widgets/similar_books_list_view.dart
@@ -13,7 +13,7 @@ class SimilarBooksListView extends StatelessWidget {
           itemBuilder: (context, index) {
             return const Padding(
               padding: EdgeInsets.symmetric(horizontal: 5),
-              child: CustomBookImage(),
+              child: CustomBookImage(imageUrl: 'https://th.bing.com/th/id/R.4ed6322d934017b04a18bb70b2b783e5?rik=qgDshq4WrsEWsw&riu=http%3a%2f%2fgetwallpapers.com%2fwallpaper%2ffull%2f8%2f2%2f5%2f415238.jpg&ehk=jJkRFoAzMCiV%2f57Kg67x0rnlJ4h2v2JUfyjXBfVgbhc%3d&risl=&pid=ImgRaw&r=0',),
             );
           }),
     );
