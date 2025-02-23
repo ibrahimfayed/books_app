737d8828094ae925bb638d6e0ed251daa32668ff
Your Name <your_email@example.com>
2025-02-23 08:28:35 +0200

    use cached network image

--- lib/features/home/presentaion/views/widgets/custom_book_image.dart
+++ lib/features/home/presentaion/views/widgets/custom_book_image.dart
@@ -1,23 +1,26 @@
 import 'package:books_app/core/utils/assets.dart';
+import 'package:books_app/core/widgets/custom_loading_indicator.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CustomBookImage extends StatelessWidget {
   const CustomBookImage({super.key, required this.imageUrl});
 final String imageUrl;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.7 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            image:  DecorationImage(
-                image: 
-                NetworkImage(
-                  imageUrl
-                  ),
-                   fit: BoxFit.fill)),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imageUrl,
+          //placeholder: (context, url) =>const CustomLoadingIndicator(),
+          errorWidget:(context, url, error) =>const Icon(FontAwesomeIcons.exclamation, color: Colors.red),
+          
       ),
+    )
     );
   }
 }
